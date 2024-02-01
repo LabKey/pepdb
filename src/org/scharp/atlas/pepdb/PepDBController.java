5945d0feb04330010be3dca5c0c6fd285741d229
Trey Chadick <tchad@labkey.com>
2024-02-01 10:13:33 -0800

    Tomcat 10 - Update namespace of Jakarta EE packages (#21)
    Co-authored-by: Adam Rauch <adam@labkey.com>

--- src/org/scharp/atlas/pepdb/PepDBController.java
+++ src/org/scharp/atlas/pepdb/PepDBController.java
@@ -45,9 +45,9 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
