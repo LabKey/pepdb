23b7f50821d76dcac64d021753f167d3feed3b6c
Trey Chadick <tchad@labkey.com>
2025-02-25 16:52:10 -0800

    Remove module code. Moved to https://github.com/LabKey/customModules (#25)

--- resources/schemas/dbscripts/postgresql/pepdb-2.00-2.10.sql
+++
@@ -1,4 +0,0 @@
-
-ALTER TABLE pepdb.peptide_pool ADD COLUMN parent_pool_id integer;
-ALTER TABLE pepdb.peptide_pool ADD CONSTRAINT FK_peptide_pool2 FOREIGN KEY(parent_pool_id) REFERENCES pepdb.peptide_pool(peptide_pool_id);
-ALTER TABLE pepdb.peptide_pool ADD COLUMN matrix_peptide_pool_id text;
